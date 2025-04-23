79ba95d0fac1151e788c239aa3aca81f6e1b0645
DeepAung <i.deepaung@gmail.com>
2025-04-23 09:28:10 +0700

    feat(pokedex): init project, config graphql file

---
+++ pokedex/server.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/99designs/gqlgen/graphql/handler"
+	"github.com/99designs/gqlgen/graphql/handler/extension"
+	"github.com/99designs/gqlgen/graphql/handler/lru"
+	"github.com/99designs/gqlgen/graphql/handler/transport"
+	"github.com/99designs/gqlgen/graphql/playground"
+	"github.com/DeepAung/apiplustech-training/pokedex/graph"
+	"github.com/go-chi/chi/v5"
+	"github.com/vektah/gqlparser/v2/ast"
+)
+
+const port = "8080"
+
+func main() {
+	r := chi.NewRouter()
+
+	srv := handler.New(graph.NewExecutableSchema(graph.Config{Resolvers: &graph.Resolver{}}))
+
+	srv.AddTransport(transport.Options{})
+	srv.AddTransport(transport.GET{})
+	srv.AddTransport(transport.POST{})
+
+	srv.SetQueryCache(lru.New[*ast.QueryDocument](1000))
+
+	srv.Use(extension.Introspection{})
+	srv.Use(extension.AutomaticPersistedQuery{
+		Cache: lru.New[string](100),
+	})
+
+	r.Handle("/", playground.ApolloSandboxHandler("GraphQL playground", "/query"))
+	r.Handle("/query", srv)
+
+	log.Printf("connect to http://localhost:%s/ for GraphQL playground", port)
+	log.Fatal(http.ListenAndServe(":"+port, r))
+}
